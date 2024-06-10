bee7ca3219bb5a90e181e352c0671dc70fb5acd9
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-06-10 14:12:33 +0300

    Update

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,20 +1,10 @@
 import "dotenv/config";
-import { Client } from "pg";
 import * as schema from "./schema"
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
 
 const sql = neon(process.env.DATABASE_URL!);
-
-export const client = new Client({
-    connectionString: process.env.Database_URL as string,   //get the database url from the environment
-})
-
-const main = async () => {
-    await client.connect();  //connect to the database
-}
-main();
 
 const db = drizzle(sql, { schema, logger: true })  //create a drizzle instance
 
