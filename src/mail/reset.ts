aa90c40523e57fc5e5f09c77de9fd890ce377cc0
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-06-17 12:22:43 +0300

    Created mail & ejs folders

---
+++ src/mail/reset.ts
@@ -0,0 +1,45 @@
+import nodemailer from 'nodemailer';
+import 'dotenv/config';
+import ejs from 'ejs';
+import path from 'path'
+
+const mailFunction = async (to: string, subject: string, user: any) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    const sendMail = async (user: any) => {
+        try {
+            // Path to the EJS template
+            const template = path.join(__dirname, '../ejs', 'reset.ejs');
+
+            // Render the EJS template with the user data
+            const content = await ejs.renderFile(template, { user });
+
+            const mailOption = {
+                from: process.env.EMAIL,
+                to,
+                subject,
+                html: content
+            };
+
+            transporter.sendMail(mailOption, function (err, info) {
+                if (err) {
+                    console.error('Error sending email:', err);
+                } else {
+                    console.log('Email sent:', info.response);
+                }
+            });
+        } catch (error) {
+            console.error('Error rendering email template:', error);
+        }
+    };
+
+    sendMail(user);
+};
+
+export default mailFunction;
